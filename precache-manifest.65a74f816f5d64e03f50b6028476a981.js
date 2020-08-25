163d24d34ab90b92f330239520687857c76afa41
Charles McQuain <charlesmcquain@gmail.com>
2020-08-24 20:51:30 -0700

    Updates

---
+++ precache-manifest.65a74f816f5d64e03f50b6028476a981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62130154d82673065822a3d2dca6e98b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "78800ab12050ab766cfc",
+    "url": "/monsters-rolodex/static/css/main.25107a4f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78800ab12050ab766cfc",
+    "url": "/monsters-rolodex/static/js/main.0ef10462.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
